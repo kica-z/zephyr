55ff1470d7c3cf59abd1ec1a303278544b7aa7be
Carlo Kirchmeier <carlo.kirchmeier@zuehlke.com>
2023-09-18 15:04:33 +0200

    WIP

--- samples/drivers/flash_disk/flash_disk_mcuboot/src/main.c
+++ samples/drivers/flash_disk/flash_disk_mcuboot/src/main.c
@@ -11,41 +11,43 @@
 #include <zephyr/dfu/flash_img.h>
 #include <zephyr/logging/log.h>
 
-#if DT_HAS_COMPAT_STATUS_OKAY(zephyr_disk_flash)
-#define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_disk_flash)
-#else
-#error Unsupported flash driver
-#define FLASH_NODE DT_INVALID_NODE
-#endif
+// #if DT_HAS_COMPAT_STATUS_OKAY(zephyr_disk_flash)
+// #define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_disk_flash)
+// #else
+// #error Unsupported flash driver
+// #define FLASH_NODE DT_INVALID_NODE
+// #endif
 
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
-static char const secondary[] = {
-#include "secondary.inc"
-};
+// static char const secondary[] = {
+// #include "secondary.inc"
+// };
 
 int main(void)
 {
 	int res = -1;
-	const struct device *const slot1_dev = DEVICE_DT_GET(FLASH_NODE);
-	uint16_t const image_size = (uint16_t)sizeof(secondary);
+	//const struct device *const slot1_dev = DEVICE_DT_GET(FLASH_NODE);
+	//uint16_t const image_size = (uint16_t)sizeof(secondary);
 	uint16_t bytes_written = 0;
+	// k_msleep(3000);
 
-	if(!boot_is_img_confirmed()) {
-		LOG_INF("Confirming image");
-	} else {
-		LOG_INF("Image already confirmed");
-	}
+	// if(!boot_is_img_confirmed()) {
+	// 	LOG_INF("Confirming image");
+	// } else {
+	// 	LOG_INF("Image already confirmed");
+	// }
 
-	while(bytes_written < image_size) {
-		LOG_INF("Progress %d/%d", bytes_written, image_size);
-		res = flash_write(slot1_dev, bytes_written, &secondary[bytes_written], 50);
-		if(res != 0) {
-			LOG_ERR("Write not possible");
-			return -1;
-		}
-		bytes_written += 50;
-	}
+	//LOG_INF("Secondary image starts with: %x", secondary[5]);
+	// while(bytes_written < image_size) {
+	// 	LOG_INF("Progress %d/%d", bytes_written, image_size);
+	// 	res = flash_write(slot1_dev, bytes_written, &secondary[bytes_written], 50);
+	// 	if(res != 0) {
+	// 		LOG_ERR("Write not possible");
+	// 		return -1;
+	// 	}
+	// 	bytes_written += 50;
+	// }
 	LOG_INF("Secondary image written successfully");
 
 	// res = flash_img_init_id(&flash_context, DT_FIXED_PARTITION_ID(DT_NODELABEL(slot1_partition)));
@@ -58,10 +60,10 @@ int main(void)
 	// 	LOG_ERR("Flash image write not possible");
 	// }
 
-	res =  boot_request_upgrade(BOOT_UPGRADE_TEST);
-	if(res != 0) {
-		LOG_ERR("Upgrade not possible");
-	}
+	// res =  boot_request_upgrade(BOOT_UPGRADE_TEST);
+	// if(res != 0) {
+	// 	LOG_ERR("Upgrade not possible");
+	// }
 
 	return 0;
 }
